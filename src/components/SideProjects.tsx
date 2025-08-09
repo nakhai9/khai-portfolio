7069bb4bfed63d175d8c11f87066c5ba04dcae41
Khai Nguyen <khai.fordev@gmail.com>
2025-08-09 17:02:57 +0700

    update

--- src/components/SideProjects.tsx
+++ src/components/SideProjects.tsx
@@ -1,4 +1,4 @@
-import Section from "./ui/Section";
+import Section from './ui/Section';
 
 type SideProjectsProps = {
   projects: Array<{
@@ -6,6 +6,7 @@ type SideProjectsProps = {
     description: string;
     imageUrl?: string;
     technologies: string[];
+    launchUrl?: string;
   }>;
 };
 const SideProjects: React.FC<SideProjectsProps> = ({ projects }) => {
@@ -16,20 +17,40 @@ const SideProjects: React.FC<SideProjectsProps> = ({ projects }) => {
           projects.map((project, index) => (
             <div
               key={index}
-              className="flex flex-col gap-2 bg-[#323443] p-2 border hover:border-amber-500 border-transparent rounded-md cursor-pointer"
+              className="flex flex-col gap-2 bg-[#323443] p-4 border hover:border-amber-500 border-transparent rounded-md cursor-pointer"
             >
-              <div className="rounded-md h-32 overflow-hidden">
+              <div className="bg-white p-2 rounded-md h-32 overflow-hidden">
                 <img
-                  className="bg-auto w-full h-full"
+                  className="w-full h-full object-contain"
                   src={project.imageUrl}
                   alt="Image"
                 />
               </div>
-              <div>
-                <h3 className="shadow-2xl mb-2 font-medium text-lg">
-                  {project.name}
-                </h3>
-                <p className="text-sm text-justify">{project.description}</p>
+              <div className="flex flex-col justify-between gap-2">
+                <div>
+                  <div className="flex flex-col mb-2">
+                    <h3 className="shadow-2xl font-medium text-lg">
+                      {project.name}
+                    </h3>
+                    <a
+                      className="text-blue-500 text-sm"
+                      href={project.launchUrl}
+                    >
+                      {project.launchUrl}
+                    </a>
+                  </div>
+                  <p className="text-sm text-justify">{project.description}</p>
+                </div>
+                <div className="flex flex-wrap gap-2 mt-2">
+                  {project.technologies.map((tech, techIndex) => (
+                    <span
+                      key={techIndex}
+                      className="bg-amber-600 p-1 px-3 rounded-xl font-bold text-xs badge"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
           ))}
--- src/data/data.ts
+++ src/data/data.ts
@@ -21,6 +21,7 @@ export const APP_DATA = {
         "A personal portfolio website to showcase my projects and skills.",
       imageUrl:
         "https://firebasestorage.googleapis.com/v0/b/firestore-project-6ae19.appspot.com/o/2025-07-26_13-51-13.png?alt=media&token=8c6de435-129f-40b6-a6d2-8e2299da3af9",
+      launchUrl: "https://khai-portfolio.vercel.app/",
     },
     {
       name: "Expense Tracker App",
@@ -29,6 +30,16 @@ export const APP_DATA = {
         "A personal finance management app designed to track and organize my income and expenses",
       imageUrl:
         "https://firebasestorage.googleapis.com/v0/b/firestore-project-6ae19.appspot.com/o/2025-07-26_15-26-19.png?alt=media&token=3467767b-b29f-4a7a-ae73-827cb4af8c7e",
+      launchUrl: "https://vitietkiem-ui.vercel.app/",
+    },
+    {
+      name: "Bill Split",
+      technologies: ["Gemini AI", "Nextjs", "TypeScript", "Tailwind CSS"],
+      description:
+        "A web application that makes splitting bills among friends and family effortless. With AI-powered bill scanning a receipt.",
+      imageUrl:
+        "https://firebasestorage.googleapis.com/v0/b/firestore-project-6ae19.appspot.com/o/2025-08-09_16-20-50.png?alt=media&token=8bec531b-3897-4e54-8e90-3bbd014cd638",
+      launchUrl: "https://billsplit-fake.vercel.app/",
     },
   ],
   emailServiceConfig: {
