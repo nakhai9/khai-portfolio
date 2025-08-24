c195ffd55eab54404f36bc848f86741adf524deb
Khai Nguyen <khai.fordev@gmail.com>
2025-08-24 23:27:14 +0700

    update

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,11 +1,9 @@
 import { APP_DATA } from "../../data/data";
 
-type FooterProps = {
-  classNames?: string;
-};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+type FooterProps = {};
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Footer: React.FC<FooterProps> = ({ classNames }) => {
+const Footer: React.FC<FooterProps> = () => {
   return (
     <div className="flex items-center mt-10 border-slate-600 border-t h-12 text-xs">
       <p>&copy; {APP_DATA.me.name} 2025</p>
